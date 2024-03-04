208db5dda8dbd071970c20f38228c766272b3969
Gerendás Zoltán <gerendas.zoltan@wmtech.hu>
2024-03-04 11:58:16 +0100

    egér+div kész

--- html/jatek.js
+++ html/jatek.js
@@ -6,12 +6,47 @@ div.style.width = "100px";
 div.style.height = "100px";
 div.style.background = "red";
 div.style.color = "blue";
-
+div.style.borderRadius = "100%";
+//div.style.border = "2px solid blue";
 document.body.appendChild(div);
 
 talalt =0
+katt = 0
 div.addEventListener('click', function (e) {
     talalt++;
+    w = div.style.width.valueOf().replace("px","") 
+    document.getElementById("katt").innerHTML = "Talált: "+talalt
+    if (talalt % 5 == 0 ) {
+        div.style.width = w*0.75+"px"
+        div.style.height= w*0.75+"px"
+    }
+/*
+    if (talalt == 10) {
+        div.style.width="40px"
+        div.style.height="40px"
+    }
+    if (talalt == 15) {
+        div.style.width="20px"
+        div.style.height="20px"
+    }
+    */
+})
+document.addEventListener('mouseup', function (e) {
+    katt++
+    document.getElementById("kattint").innerHTML="Kattintva: "+katt
 })
 
-var h = setInterval(function(){},1000)
+
+var h = setInterval(function(){
+    div.style.left = Math.floor(Math.random()*400)+'px'
+    div.style.top = Math.floor(Math.random()*400)+'px'
+},1000)
+
+
+
+
+
+
+
+
+
